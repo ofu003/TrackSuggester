79dcb7f907cffa0d5266c9fa238ea62ac2ae458e
ofu997 <ofu997@gmail.com>
2017-02-17 13:50:22 -0800

    Add variables and branching to scripts.js, error messages to html, indenting on css

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,53 @@
 
 $(function(){
   $("form#Form").submit(function(event){
-    var sign = $("#signbox").val();
-    var age = $("#age").val();
-    var gender = $("#genderbox").val();
+    var graphicDesign = $("#q1").val();
+    var workingEnv = $("#q2").val();
+    var hobbies = $("#q13").val();
+    var appsVsWeb = $("#q4").val();
+    var dynamicContent = $("#q5").val();
 
     //if gender and age are not empty, match to celeb
-    if (gender!==""&&age!==""){
-      if (sign==="aquarius"){
-        $("#showAshton").show();
+    if (graphicDesign!=="blank"&&workingEnv!=="blank"&&hobbies!=="blank"&&appsVsWeb!=="blank"&&dynamicContent!=="blank"){
+      if (graphicDesign==="1"&&hobbies==="yes"){
+        $("#showCSSDesign").show();
         }
-      else if (sign==="pisces"){
+      else if (dynamicContent==="y"){
         $("#showEva").show();
+        }
+      else {
+        $("#showJavaAndroid").show();
         }
       }
     //else show alert, specific missing info
     else{
-      alert("There is missing information and we can't accurately match you");
-
-      if (gender){
+      alert("There is missing information and we may not accurately match you");
+      if (graphicDesign){
         }
       else{
-        $("#errorGender").show();
+        $("#errorq1").show();
         }
-
-      if (age){
+      if (workingEnv){
         }
-      else {
-        $("#errorAge").show();
+      else{
+        $("#errorq2").show();
         }
-     }
-  event.preventDefault();
+      if (hobbies){
+        }
+      else{
+        $("#errorq3").show();
+        }
+      if (appsVsWeb){
+        }
+      else{
+        $("#errorq4").show();
+        }
+      if (dynamicContent){
+        }
+      else{
+        $("#errorq5").show();
+        }
+      }
+    event.preventDefault();
+    });
   });
-
-});
