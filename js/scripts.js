02e7b386eeb53950d433156e5c8277dbd77dd117
ofu997 <ofu997@gmail.com>
2017-02-17 14:40:59 -0800

    Indent html and css, correct missing information portion of branches

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,17 @@ $(function(){
   $("form#Form").submit(function(event){
     var graphicDesign = $("#q1").val();
     var workingEnv = $("#q2").val();
-    var hobbies = $("#q13").val();
+    var hobbies = $("#q3").val();
     var appsVsWeb = $("#q4").val();
     var dynamicContent = $("#q5").val();
 
-    //if gender and age are not empty, match to celeb
+    //if fields are all filled, suggest track
     if (graphicDesign!=="blank"&&workingEnv!=="blank"&&hobbies!=="blank"&&appsVsWeb!=="blank"&&dynamicContent!=="blank"){
-      if (graphicDesign==="1"&&hobbies==="yes"){
+      if (graphicDesign==="1"&&hobbies==="y"){
         $("#showCSSDesign").show();
         }
-      else if (dynamicContent==="y"){
-        $("#showEva").show();
+      else if (dynamicContent==="y"&&appsVsWeb==="websites"){
+        $("#showRubyRails").show();
         }
       else {
         $("#showJavaAndroid").show();
@@ -22,27 +22,27 @@ $(function(){
     //else show alert, specific missing info
     else{
       alert("There is missing information and we may not accurately match you");
-      if (graphicDesign){
+      if (graphicDesign!=="blank"){
         }
       else{
         $("#errorq1").show();
         }
-      if (workingEnv){
+      if (workingEnv!=="blank"){
         }
       else{
         $("#errorq2").show();
         }
-      if (hobbies){
+      if (hobbies!=="blank"){
         }
       else{
         $("#errorq3").show();
         }
-      if (appsVsWeb){
+      if (appsVsWeb!=="blank"){
         }
       else{
         $("#errorq4").show();
         }
-      if (dynamicContent){
+      if (dynamicContent!=="blank"){
         }
       else{
         $("#errorq5").show();
